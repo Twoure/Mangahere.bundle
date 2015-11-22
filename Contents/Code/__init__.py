ff95a41628711bc21e9d65f5ca1884207b550952
Twoure <twoure@aol.com>
2015-11-22 14:01:43 -0600

    cleaned code

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -4,12 +4,19 @@ ROOT_URL = 'http://www.mangahere.co'
 
 MANGAHERE_QUERY = ROOT_URL + '/search.php?name_method=cw&name=%s&author_method=cw&artist_method=cw&released_method=eq&advopts=1'
 
+"""
+USER_AGENT = (
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) '
+    'AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/31.0.1650.63 Safari/537.36'
+    )
+"""
+
 randomArt = Util.RandomInt(1, 3)
 ART = 'art-default_' + str(randomArt) + '.png'
 ICON = 'icon-default.png'
 
 ####################################################################################################
-
 def Start():
     Plugin.AddViewGroup('ImageStream', viewMode='Pictures', mediaType='items')
     Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
@@ -20,23 +27,26 @@ def Start():
     ObjectContainer.view_group = 'InfoList'
     DirectoryObject.thumb = R(ICON)
 
-    HTTP.CacheTime = 0  # 0 sec cache time
+    HTTP.CacheTime = 0
+    #HTTP.Headers['User-Agent'] = USER_AGENT
 
 ####################################################################################################
 @handler(PLUGIN_PREFIX, PLUGIN_TITLE, ICON, ART)
 def MainMenu():
     oc = ObjectContainer(view_group='List')
+
     oc.add(DirectoryObject(key=Callback(AlphabetList), title='Alphabets'))
     oc.add(DirectoryObject(key=Callback(GenreList), title='Genres'))
     oc.add(PrefsObject(title='Preferences'))
     oc.add(InputDirectoryObject(key=Callback(Search), title='Search Manga', prompt='Search Manga'))
+
     return oc
 
 ####################################################################################################
-
 @route(PLUGIN_PREFIX + '/ValidatePrefs')
 def ValidatePrefs():
-    # Set the sort option for displaying Manga list
+    """Set the sort option for displaying Manga list"""
+
     if Prefs['sort_opt'] == 'A-Z':
         Dict['s_opt'] = 'name.az'
     elif Prefs['sort_opt'] == 'Z-A':
@@ -51,13 +61,16 @@ def ValidatePrefs():
         Dict['s_opt'] = 'views.az'
     elif Prefs['sort_opt'] == 'Latest Updated':
         Dict['s_opt'] = 'last_chapter_time.za'
+
     Dict.Save()
 
 ####################################################################################################
-
 @route(PLUGIN_PREFIX + '/alphabets')
 def AlphabetList():
+    """Generate #, ABC list"""
+
     oc = ObjectContainer(title2='Manga By #, A-Z', view_group='List')
+
     for pname in ['#'] + map(chr, range(ord('A'), ord('Z')+1)):
         oc.add(DirectoryObject(
             key=Callback(DirectoryList, page=1, pname=pname.lower() if not '#' else '9', ntitle=pname),
@@ -69,9 +82,10 @@ def AlphabetList():
     return oc
 
 ####################################################################################################
-
 @route(PLUGIN_PREFIX + '/genres')
 def GenreList():
+    """Generate Genre List"""
+
     url = ROOT_URL + '/directory/'  # set url for populating genres array
     html = HTML.ElementFromURL(url)  # formate url response into html so we can use xpath
 
@@ -92,15 +106,19 @@ def GenreList():
     return oc
 
 ####################################################################################################
-
 @route(PLUGIN_PREFIX + '/directory/{pname}')
 def DirectoryList(page, pname, ntitle):
+    """Get list of Mangas"""
+
     url = ROOT_URL + '/%s/%s.htm?%s' %(pname, page, Dict['s_opt'])
     Log(Dict['s_opt'])
+
     html = HTML.ElementFromURL(url)
+
     pages = html.xpath('//div[@class="next-page"]//a[@href]')
     total_pages = pages[int(len(pages))-2].text
     Log(total_pages)
+
     main_title = '%s: Page %s of %s' % (str(ntitle), str(page), str(total_pages))
 
     oc = ObjectContainer(title2=main_title, view_group='List')
@@ -112,7 +130,9 @@ def DirectoryList(page, pname, ntitle):
         url = node2.get('href')
         if url[-1] == '/':
             url = url[:-1]
+
         manga = url.rsplit('/', 2)[-1]
+
         oc.add(DirectoryObject(
             key=Callback(MangaPage, manga=manga, title=title),
             title=title,
@@ -123,6 +143,7 @@ def DirectoryList(page, pname, ntitle):
     if nextpg_node:
         nextpg = int(nextpg_node[0].get('href').split('.')[0])
         Log.Debug('NextPage = %d' % nextpg)
+
         oc.add(NextPageObject(
             key=Callback(DirectoryList, page=nextpg, pname=pname, ntitle=ntitle),
             title='Next Page>>'
@@ -131,24 +152,30 @@ def DirectoryList(page, pname, ntitle):
     return oc
 
 ####################################################################################################
-
 @route(PLUGIN_PREFIX + '/search')
 def Search(query=''):
+    """Search for Manga"""
+
     Log.Debug('Search ' + query)
     url = MANGAHERE_QUERY % String.Quote(query, usePlus=True)
+
     html = HTML.ElementFromURL(url)
+
     oc = ObjectContainer(view_group='List')
+
     for node in html.xpath('//div[@class="result_search"]//dt'):
         url = node.xpath('./a')[0].get('href')
         title = node.xpath('./a')[0].text
         if url[-1] == '/':
             url = url[:-1]
+
         manga = url.rsplit('/', 2)[-1]
+
         oc.add(DirectoryObject(key=Callback(MangaPage, manga=manga, title=title), title=title))
+
     return oc
 
 ####################################################################################################
-
 @route(PLUGIN_PREFIX + '/manga/{manga}')
 def MangaPage(manga, title):
     oc = ObjectContainer(title2=title, view_group='List')
